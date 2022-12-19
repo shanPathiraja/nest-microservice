065b0455b5112e32ad4c64eee1a639f1fb5c8212
shan pathiraja <shanb11170@gmail.com>
2022-12-19 23:15:30 +0530

    initialize kafka notification service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FileModule } from './file/file.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloFederationDriver, ApolloFederationDriverConfig} from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { join } from 'path';
 import * as process from 'process';
 
